b68b559851e373512450bf3fed2bcf567fb45572
Les Orchard <lorchard@mozilla.com>
2024-07-31 14:29:23 -0700

    quick fix for logger

--- src/services/base.ts
+++ src/services/base.ts
@@ -3,10 +3,12 @@ import { BaseApp, BaseLogger } from "../app/types";
 
 export class BaseService {
   app: BaseApp;
-  log: BaseLogger;
 
   constructor(app: BaseApp) {
     this.app = app;
-    this.log = app.logging.child({ module: this.constructor.name });
+  }
+
+  get log() {
+    return this.app.logging.child({ module: this.constructor.name });
   }
 }
