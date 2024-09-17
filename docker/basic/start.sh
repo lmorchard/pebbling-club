affc5e63d12e02f155e53795f392801f6ac17799
Les Orchard <lorchard@mozilla.com>
2024-09-17 16:00:28 -0700

    tweak basic docker start.sh script

--- docker/basic/start.sh
+++ docker/basic/start.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-docker run --rm -d --name pebbling-club -v `pwd`/data:/var/data -p 17000:8081 -e SITE_URL=https://pebbling.lmorchard.com -e LOG_LEVEL=debug lmorchard/pebbling-club
+docker run \
+  -d \
+  --name pebbling-club \
+  --restart unless-stopped \
+  -v `pwd`/data:/var/data \
+  -p 17000:8081 \
+  -e SITE_URL=https://pebbling.lmorchard.com \
+  -e LOG_LEVEL=debug \
+  lmorchard/pebbling-club
