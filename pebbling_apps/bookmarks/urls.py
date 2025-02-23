f965b86f75443c755b598f028f318780c4a511dc
Les Orchard <lmorchard@github.com>
2025-02-23 12:12:16 -0800

    quick bugfix for new bookmark link

--- pebbling_apps/bookmarks/urls.py
+++ pebbling_apps/bookmarks/urls.py
@@ -12,9 +12,9 @@ app_name = "bookmarks"
 
 urlpatterns = [
     path("bookmarks/", BookmarkListView.as_view(), name="list"),
-    path("add", BookmarkCreateView.as_view(), name="add"),
-    path("bookmark/<int:pk>/edit", BookmarkUpdateView.as_view(), name="edit"),
-    path("bookmark/<int:pk>/delete", BookmarkDeleteView.as_view(), name="delete"),
+    path("bookmarks/new", BookmarkCreateView.as_view(), name="add"),
+    path("bookmarks/<int:pk>/edit", BookmarkUpdateView.as_view(), name="edit"),
+    path("bookmarks/<int:pk>/delete", BookmarkDeleteView.as_view(), name="delete"),
     path("t/", TagListView.as_view(), name="tag_list"),
     path("t/<str:tag_name>", TagDetailView.as_view(), name="tag_detail"),
 ]
