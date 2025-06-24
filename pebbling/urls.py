bba0ce12aa0743ce9acf164a9bbd4ed68684b1ab
Les Orchard <lorchard@mozilla.com>
2025-06-24 11:26:55 -0700

    make format

--- pebbling/urls.py
+++ pebbling/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
 # Include debug toolbar URLs in debug mode
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns = [
         path("__debug__/", include(debug_toolbar.urls)),
     ] + urlpatterns
